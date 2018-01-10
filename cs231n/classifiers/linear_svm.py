12ad7ad46aaa9b33628b50533515211cd3674a21
joshbernardo <jbernardo@commschool.org>
2018-01-10 12:35:25 -0500

    Update linear_svm.py

--- cs231n/classifiers/linear_svm.py
+++ cs231n/classifiers/linear_svm.py
@@ -9,8 +9,8 @@ def svm_loss_naive(W, X, y, reg):
   of N examples.
 
   Inputs:
-  - W: A numpy array of shape (D, C) containing weights.
-  - X: A numpy array of shape (N, D) containing a minibatch of data.
+  - W: A numpy array of shape (D (each pixel), C (each class)) containing weights.
+  - X: A numpy array of shape (N (each image), D (each pixel)) containing a minibatch of data.
   - y: A numpy array of shape (N,) containing training labels; y[i] = c means
     that X[i] has label c, where 0 <= c < C.
   - reg: (float) regularization strength
@@ -34,13 +34,18 @@ def svm_loss_naive(W, X, y, reg):
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
         loss += margin
+        dW[:, j] += X[i, :]
+        dW[:, y[i]] -= X[i, :]
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
+  dW /= num_train
 
   # Add regularization to the loss.
   loss += reg * np.sum(W * W)
+
+  dW += 2*W*reg
 
   #############################################################################
   # TODO:                                                                     #
@@ -50,7 +55,6 @@ def svm_loss_naive(W, X, y, reg):
   # loss is being computed. As a result you may need to modify some of the    #
   # code above to compute the gradient.                                       #
   #############################################################################
-
 
   return loss, dW
 
@@ -69,7 +73,28 @@ def svm_loss_vectorized(W, X, y, reg):
   # Implement a vectorized version of the structured SVM loss, storing the    #
   # result in loss.                                                           #
   #############################################################################
-  pass
+
+  num_classes = W.shape[1]
+  num_train = X.shape[0]
+
+  for i in range(num_train) -> X[num_train]
+  for i in range(num_classes) -> W[:, num_classes]
+
+  scores = dot(X, W)
+
+  
+  for i in range(num_train):
+    scores = X[i].dot(W)
+    correct_class_score = scores[y[i]]
+    for j in range(num_classes):
+      if j == y[i]:
+        continue
+      margin = scores[j] - correct_class_score + 1 # note delta = 1
+      if margin > 0:
+        loss += margin
+        dW[:, j] += X[i, :]
+        dW[:, y[i]] -= X[i, :]
+
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
