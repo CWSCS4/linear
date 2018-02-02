c9c85beedac7aac3c9344a9e4bef71fe2cea4d16
Alex Lehner <alexxlehner@gmail.com>
2018-02-02 00:52:38 -0500

    Implemented SVM functionality and gradient descent prediction

--- cs231n/classifiers/linear_svm.py
+++ cs231n/classifiers/linear_svm.py
@@ -34,23 +34,18 @@ def svm_loss_naive(W, X, y, reg):
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
         loss += margin
+        dW[:,j] += X[i]
+        dW[:,y[i]] -= X[i]
+
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
+  dW /= num_train
 
   # Add regularization to the loss.
   loss += reg * np.sum(W * W)
-
-  #############################################################################
-  # TODO:                                                                     #
-  # Compute the gradient of the loss function and store it dW.                #
-  # Rather that first computing the loss and then computing the derivative,   #
-  # it may be simpler to compute the derivative at the same time that the     #
-  # loss is being computed. As a result you may need to modify some of the    #
-  # code above to compute the gradient.                                       #
-  #############################################################################
-
+  dW += 2 * reg * W
 
   return loss, dW
 
@@ -69,7 +64,13 @@ def svm_loss_vectorized(W, X, y, reg):
   # Implement a vectorized version of the structured SVM loss, storing the    #
   # result in loss.                                                           #
   #############################################################################
-  pass
+  scores = X.dot(W)
+  correct_class_scores = np.reshape(scores[np.arange(X.shape[0]), y], (-1, 1))
+  margins = scores - correct_class_scores + 1
+  margins[np.arange(X.shape[0]), y] = 0
+  countedMargins = margins[margins > 0]
+  loss = sum(countedMargins) / X.shape[0]
+  loss += reg * np.sum(W * W)
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
@@ -84,7 +85,15 @@ def svm_loss_vectorized(W, X, y, reg):
   # to reuse some of the intermediate values that you used to compute the     #
   # loss.                                                                     #
   #############################################################################
-  pass
+
+  #Kevin suggested that I convert the bolleanMargins array to floats
+  booleanMargins = np.transpose((margins>0)).astype(np.float64)
+
+  columnSums = np.sum(booleanMargins, axis = 0)
+  booleanMargins[y, np.arange(X.shape[0])] = columnSums * -1
+  dW = np.transpose(np.dot(booleanMargins, X)) / X.shape[0]
+
+  dW += 2 * reg * W
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
